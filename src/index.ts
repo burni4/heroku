edb50890353c34a39b407e322235d4ea41971084
burni4 <artemnarchyk@gmail.com>
2022-10-12 13:13:35 +0300

    second commit (test)

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ const port = 3000
 
 app.get('/', (req: Request, res: Response) => {
 
+    // com
+
     const today = new Date();
     const dd = String(today.getDate()).padStart(2, '0');
     const mm = String(today.getMonth() + 1).padStart(2, '0');
