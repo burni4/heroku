b2f63352658c665695fd1a2abe54ae9c45a9d36c
burni4 <artemnarchyk@gmail.com>
2022-10-25 20:49:00 +0300

    fix

--- src/repositories/products-repository-db.ts
+++ src/repositories/products-repository-db.ts
@@ -5,31 +5,33 @@ export type ProductType = {
     title: string
 }
 
+const productsCollection = client.db("shop").collection<ProductType>("products");
+
 export const productsRepositoryInMemory = {
     async findProducts(searchString: string | null | undefined): Promise<ProductType[]>{
-        if(searchString){
-            return client.db("shop").collection<ProductType>("products").find({title: {$regex: searchString}}).toArray()
-            } else {
-            return client.db("shop").collection<ProductType>("products").find({}).toArray()
+        let filter: any = {};
+        if (searchString) {
+            filter.title = {$regex: searchString}
         }
-        },
+        return productsCollection.find(filter).toArray()
+    },
     async createProduct(title: string): Promise<ProductType>{
             const newProduct = {id: +(new Date()), title};
-            const result = await client.db("shop").collection<ProductType>("products").insertOne(newProduct)
+            const result = await productsCollection.insertOne(newProduct)
             return newProduct;
         },
     async findProductByID(id: number): Promise<ProductType | null> {
-        return await client.db("shop").collection<ProductType>("products").findOne({id: id});
+        return await productsCollection.findOne({id: id});
     },
     async updateProductByID(id: number, title: string): Promise<boolean>{
 
-        const result = await client.db("shop").collection<ProductType>("products").updateOne({id: id}, {$set: {title: title}})
+        const result = await productsCollection.updateOne({id: id}, {$set: {title: title}})
 
         return result.matchedCount === 1
 
     },
     async deleteProductByID(id: number): Promise<boolean>{
-        const result = await client.db("shop").collection<ProductType>("products").deleteOne({id: id})
+        const result = await productsCollection.deleteOne({id: id})
         return result.deletedCount === 1;
     },
 }
